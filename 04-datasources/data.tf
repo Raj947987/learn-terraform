27d741365dc82c4da813124a841c77ed1143b070
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-06-15 20:34:11 +0530

    Include_tasks in to another ansible file

--- 04-datasources/data.tf
+++ 04-datasources/data.tf
@@ -18,3 +18,9 @@ data "aws_security_group" "selected" {
 output "sgid" {
   value = data.aws_security_group.selected.id
 }
+
+data "aws_ami" "example" {
+  executable_users = ["amazon"]
+  most_recent = true
+  name_regex = "Centos-8-DevOps-practice"
+}
