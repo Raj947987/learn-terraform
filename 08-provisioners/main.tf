18821573c3b4d810977ed7f88c9634c42842526e
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-06-16 18:46:51 +0530

    Include_tasks in to another ansible file

--- 08-provisioners/main.tf
+++ 08-provisioners/main.tf
@@ -6,23 +6,23 @@ resource "aws_instance" "web" {
   tags = {
     Name = "HelloWorld"
   }
-    provisioner "remote-exec" {
-      connection {
-        type     = "ssh"
-        user     = "centos"
-        password = "DevOps321"
-        host     = self.public_ip
-      }
-      inline = [
-        "sudo labauto ansible",
-        "ansible-pull -i localhost, -U https://github.com/Raj947987/roboshop-ansible.git main.yml -e env=dev -e role_name=frontend",
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+    inline = [
+      "sudo labauto ansible",
+      "ansible-pull -i localhost, -U https://github.com/Raj947987/roboshop-ansible.git main.yml -e env=dev -e role_name=frontend",
     ]
     }
 
 }
 
-data "aws_ami" "example" {
-  owners      = ["973714476881"]
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
+  data "aws_ami" "example" {
+    owners      = ["973714476881"]
+    most_recent = true
+    name_regex  = "Centos-8-DevOps-Practice"
 }
