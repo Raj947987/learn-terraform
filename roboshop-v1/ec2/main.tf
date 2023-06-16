95a64b25a5f2440181024ab00bec02db33695368
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-06-16 20:05:26 +0530

    Include_tasks in to another ansible file

--- roboshop-v1/ec2/main.tf
+++ roboshop-v1/ec2/main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "web" {
     }
 
     inline = [
-      "sudo labauto ansible",
+      "labauto ansible",
       "ansible-pull -i localhost, -U https://github.com/Raj947987/roboshop-ansible  main.yml -e env=dev -e role_name=${var.name}"
 
     ]
