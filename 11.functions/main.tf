7a1634b70f2fd27cfcd460543fdcd7fd9fe0d0f9
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-06-18 19:53:58 +0530

    Include_tasks in to another ansible file

--- 11.functions/main.tf
+++ 11.functions/main.tf
@@ -4,3 +4,10 @@ variable "class" {
 output "class" {
   value = upper(var.class)
 }
+
+variable "fruits" {
+  default = ["Apple","Banana","Orange"]
+}
+output "fruits_count" {
+  value = length(var.fruits)
+}
