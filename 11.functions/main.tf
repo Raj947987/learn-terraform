b361837cc877d851db6fb9e01524c2af48d9b4fe
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-06-18 20:05:43 +0530

    Include_tasks in to another ansible file

--- 11.functions/main.tf
+++ 11.functions/main.tf
@@ -11,3 +11,22 @@ variable "fruits" {
 output "fruits_count" {
   value = length(var.fruits)
 }
+variable "classes" {
+  default = {
+    devops = {
+      name = "devops"
+      topics= ["Terraform","Jenkins","Docker"]
+  }
+    aws= {
+      name = "aws"
+      topics= ["Ec2","Delta"]
+    }
+  }
+}
+
+output "devops_topics" {
+  value = var.classes["devops"]["topics"]
+}
+output "aws_topics" {
+  value = var.classes["aws"]["topics"]
+}
