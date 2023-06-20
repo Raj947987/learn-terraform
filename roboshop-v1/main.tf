636ad9de6565440293231bdaa451fdad7bb8d970
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-06-20 09:46:51 +0530

    Include_tasks in to another ansible file

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,41 +1,72 @@
-module "frontend" {
+module "instances" {
+  for_each = var.instances
   source = "./ec2"
-  name = "frontend"
-}
-module "mongodb" {
-  source = "./ec2"
-  name = "mongodb"
+  name = each.key
 }
 
-module "catalogue" {
-  source = "./ec2"
-  name = "catalogue"
+variable "instances" {
+  default = {
+
+    frontend = {}
+    mongodb = {}
+    catalogue = {}
+    redis = {}
+    cart = {}
+    user = {}
+    shipping = {}
+    rabbitmq = {}
+    mysql = {}
+    payment = {}
+  }
 }
-module "redis" {
-  source = "./ec2"
-  name = "redis"
-}
-module "user" {
-  source = "./ec2"
-  name = "user"
-}
-module "mysql" {
-  source = "./ec2"
-  name = "mysql"
-}
-module "cart" {
-  source = "./ec2"
-  name = "cart"
-}
-module "shipping" {
-  source = "./ec2"
-  name = "shipping"
-}
-module "rabbitmq" {
-  source = "./ec2"
-  name = "rabbitmq"
-}
-module "payment" {
-  source = "./ec2"
-  name = "payment"
-}
+
+
+
+
+
+
+
+
+
+
+#module "frontend" {
+#  source = "./ec2"
+#  name = "frontend"
+#}
+#module "mongodb" {
+#  source = "./ec2"
+#  name = "mongodb"
+#}
+#
+#module "catalogue" {
+#  source = "./ec2"
+#  name = "catalogue"
+#}
+#module "redis" {
+#  source = "./ec2"
+#  name = "redis"
+#}
+#module "user" {
+#  source = "./ec2"
+#  name = "user"
+#}
+#module "mysql" {
+#  source = "./ec2"
+#  name = "mysql"
+#}
+#module "cart" {
+#  source = "./ec2"
+#  name = "cart"
+#}
+#module "shipping" {
+#  source = "./ec2"
+#  name = "shipping"
+#}
+#module "rabbitmq" {
+#  source = "./ec2"
+#  name = "rabbitmq"
+#}
+#module "payment" {
+#  source = "./ec2"
+#  name = "payment"
+#}
